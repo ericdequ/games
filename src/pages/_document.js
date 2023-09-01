ee7588920d4be8a2c216016d91a5a0823781d85e
ericdequ <dequevedo.eric@ufl.edu>
2023-09-01 10:59:42 -0700

    seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <meta name="google-site-verification" content="At3RZ4YGQtXrtPbI2UKNk1aKwGJ5P7goMttGiLMTaT4" />
+      
+      <Head/>
       <body>
         <Main />
         <NextScript />
