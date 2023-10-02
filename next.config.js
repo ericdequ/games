747cc2ca69bc3e065cf9e2095c695d358a8aae15
ericdequ <dequevedo.eric@ufl.edu>
2023-10-02 15:02:22 -0400

    games thumbnails backlink webp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['www.quantumcybersolutions.com', 'quantumcybersolutions.com'],
+  },
 }
 
 module.exports = nextConfig
